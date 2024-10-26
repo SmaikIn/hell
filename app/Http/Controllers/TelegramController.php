8a8920fd5f7436fcbbb289cfafb17b527007df98
nikolay <www_nick2590@mail.ru>
2024-10-26 16:19:42 +0300

    small fix

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Events\SendToTelegramEvent;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Event;
 use Telegram\Bot\Api;
 
@@ -11,7 +12,8 @@ class TelegramController extends Controller
 
     public function __construct(
         private readonly Api $telegram
-    ){
+    )
+    {
     }
 
     public function index()
@@ -19,20 +21,20 @@ class TelegramController extends Controller
 
         $event = \App\Models\Event::find(2);
         Event::dispatch(new SendToTelegramEvent(
-           title: $event->title,
+            title: $event->title,
             description: $event->description,
             color: $event->color,
-            start: $event->start_time,
-            end: $event->end_time,
+            start: Carbon::parse($event->start_time),
+            end: Carbon::parse($event->end_time),
             chat_id: -1002472839215,
         ));
         //$response = $this->telegram->getUpdates();
-       /* $this->telegram->sendMessage(
-            [
-                'chat_id' => '-1002472839215',
-                'text' => 'девил чорт',
-            ]
-        );*/
+        /* $this->telegram->sendMessage(
+             [
+                 'chat_id' => '-1002472839215',
+                 'text' => 'девил чорт',
+             ]
+         );*/
         //dump($response);
     }
 }

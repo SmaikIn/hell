47e33e39b832f92c3fb2e67de9bf689bd3d71b25
nikolay <www_nick2590@mail.ru>
2024-10-26 16:48:47 +0300

    add chat

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -28,6 +28,14 @@ class TelegramController extends Controller
             end: Carbon::parse($event->end_time),
             chat_id: -1002472839215,
         ));
+        Event::dispatch(new SendToTelegramEvent(
+            title: $event->title,
+            description: $event->description,
+            color: $event->color,
+            start: Carbon::parse($event->start_time),
+            end: Carbon::parse($event->end_time),
+            chat_id: -1002224260721,
+        ));
         //$response = $this->telegram->getUpdates();
         /* $this->telegram->sendMessage(
              [
